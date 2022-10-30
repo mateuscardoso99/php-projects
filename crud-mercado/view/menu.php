b8fe6ff4dad39381fda6a5484b02b35ecf880bd6
Mateus Vargas <mat358591@gmail.com>
2022-10-30 10:32:01 -0300

    Create menu.php

---
+++ crud-mercado/view/menu.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle de Produtos</title>
+</head>
+<body>
+    <div>
+        <h1>Controle de produtos:</h1>
+        <ul>
+            <li>
+                <a href="cadastroProduto.php">Cadastrar</a>
+            </li>
+            <li>
+                <a href="listarProduto.php">Ver produtos</a>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
